1db5c440f8be39d3ed1411bb95b854ff88fa588a
toavina77 <m.randrianjafy@etechconsulting-mg.com>
2025-01-14 22:26:39 +0300

    starting project

--- Backend/manage.py
+++ Backend/manage.py
@@ -8,13 +8,19 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticket_system.settings')
     try:
+        from django.core.management.commands.runserver import Command as runserver
+        runserver.default_port = '8000'
+
         from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
